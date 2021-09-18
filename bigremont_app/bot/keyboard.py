703e3378730ba9d0332c0aca6a26225f4c98323b
Дмитрий <55105946+isys35@users.noreply.github.com>
2021-09-18 19:40:51 +0300

    add keyboard

---
+++ bigremont_app/bot/keyboard.py
@@ -0,0 +1,56 @@
+from telebot.types import ReplyKeyboardRemove
+
+
+class State:
+    __state = None
+
+    @property
+    def state(self):
+        return self.__state
+
+    def __init__(self, state):
+        self.__state = state
+        self.__state.context = self
+
+    def transition_to(self, state):
+        self.__state = state
+        self.__state.context = self
+
+
+def keyboard(method):
+    def decorator(self, *args):
+        self.kb_data = []
+        method(self, *args)
+        return self.get_keyboard(self.kb_data)
+
+    return decorator
+
+
+class BotKeyboard(State):
+    kb_data = None
+
+    def __init__(self, context):
+        State.__init__(self, context)
+
+    def row(self, *args):
+        button_row = [*args]
+        self.kb_data.append(button_row)
+
+    def get_keyboard(self, buttons):
+        """
+        Вызывает одноименную функцию из созданного state
+        :param buttons:
+        :return:
+        """
+        return self.state.get_keyboard(buttons)
+
+    @keyboard
+    def main(self) -> None:
+        """
+        Функция генерации клавиатуры главного меню.
+        :return: None
+        """
+        self.row('🏠 Выбрать объект')
+
+    def clear_keyboard(self):
+        return ReplyKeyboardRemove()
