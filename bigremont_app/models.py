09df72531a68c3833885a97c57f9e316f763606b
Дмитрий <55105946+isys35@users.noreply.github.com>
2021-09-19 13:58:59 +0300

    add RemontObjectModel

--- bigremont_app/models.py
+++ bigremont_app/models.py
@@ -4,3 +4,7 @@ from django.db import models
 class TelegramUser(models.Model):
     username = models.CharField(max_length=100, db_index=True, default=None)
     state = models.CharField(max_length=200, default='/')
+
+
+class RemontObject(models.Model):
+    name = models.CharField(max_length=100, db_index=True)
