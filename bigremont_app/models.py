fcf0b98e7329ac8fedefc50b30ba3b698de0643c
Дмитрий <55105946+isys35@users.noreply.github.com>
2021-09-19 14:55:21 +0300

    add material model

--- bigremont_app/models.py
+++ bigremont_app/models.py
@@ -15,3 +15,21 @@ class RemontObject(models.Model):
     class Meta:
         verbose_name = 'Объект'
         verbose_name_plural = 'Объекты'
+
+
+class Material(models.Model):
+    UNIT_OF_MEASUREMENT = [
+        ('шт', 'шт'),
+        ('лист', 'лист'),
+        ('пачка', 'пачка'),
+        ('рулон', 'рулон'),
+        ('мешок', 'мешок'),
+        ('ведро', 'ведро'),
+        ('м', 'м'),
+    ]
+    name = models.CharField(max_length=250, db_index=True, verbose_name='Наименование материала')
+    unit_measurement = models.CharField(max_length=100, verbose_name='Единица измерения')
+
+    class Meta:
+        verbose_name = 'Материал'
+        verbose_name_plural = 'Материалы'
