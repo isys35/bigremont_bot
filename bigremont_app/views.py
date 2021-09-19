58f31b5efd3d66f98078eceb2ceb19759fab47c7
Дмитрий <55105946+isys35@users.noreply.github.com>
2021-09-19 13:59:18 +0300

    finish webhook

--- bigremont_app/views.py
+++ bigremont_app/views.py
@@ -8,7 +8,9 @@ from django.views.decorators.http import require_http_methods
 from pydantic import ValidationError
 
 from bigremont_app.bot.bot import Bot
+from bigremont_app.bot.router import Router
 from bigremont_app.bot.telegram_context import TelegramContext
+from bigremont_app.bot.urls import urls
 from bigremont_app.serializers import UpdateTelegramSerializer
 
 
@@ -23,22 +25,6 @@ def webhook(request: WSGIRequest):
         return JsonResponse(data=error_data, status=400, safe=False)
     telegram_context = TelegramContext(settings.TELEGRAM_TOKEN)
     bot = Bot(telegram_context, update)
-    return JsonResponse(data=json_data, status=200)
-
-# class WebHook(APIView):
-#     """
-#     Вебхук для телеграмма
-#     """
-#     serializer_class = UpdateSerializer
-#     permission_classes = [AllowAny]
-#
-#     def post(self, request):
-#         serializer = self.serializer_class(data=request.data)
-#         serializer.is_valid(raise_exception=True)
-#         if not serializer.data.get('message') and not serializer.data.get('callback_query'):
-#             return Response({"success": False, 'error':'Not message' }, status=status.HTTP_400_BAD_REQUEST)
-#         telegram_context = TelegramContext(settings.TELEGRAM_TOKEN)
-#         bot = Bot(telegram_context, serializer)
-#         router = Router(urls)
-#         router.url_dispatcher(bot)
-#         return Response({"success": True}, status=status.HTTP_200_OK)
+    router = Router(urls)
+    router.url_dispatcher(bot)
+    return JsonResponse(data={"success": True}, status=200)
