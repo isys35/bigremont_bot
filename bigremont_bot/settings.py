a6f9e1228c683a260c32aa018ac579555591d9d0
Дмитрий <55105946+isys35@users.noreply.github.com>
2021-09-22 21:23:04 +0300

    add docker

--- bigremont_bot/settings.py
+++ bigremont_bot/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-!39tw1=#y_sbu+l1xqqbkxh#sokq9q-o41m(shse37550@#pzp
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
